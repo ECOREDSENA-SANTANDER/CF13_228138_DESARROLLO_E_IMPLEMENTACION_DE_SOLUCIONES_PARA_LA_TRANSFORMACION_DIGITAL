635f1008395eeaa09120995e59a0f83a040b5a7e
lizmanchego <karinamanchego0416@gmail.com>
2024-03-07 12:42:39 -0500

    Menu, Sintesis, Glosario

--- src/config/global.js
+++ src/config/global.js
@@ -34,49 +34,252 @@ export default {
         numero: '1',
         titulo: 'Modelamiento y gestión de bases de datos',
         desarrolloContenidos: true,
-        subMenu: [],
+        subMenu: [
+          {
+            numero: '1.1',
+            titulo: 'Metodologías de diseño y normalización Principios de ACID',
+            hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo: 'Técnicas de almacenamiento de datos y consultas',
+            hash: 't_1_2',
+          },
+          {
+            numero: '1.3',
+            titulo: 'Estándares técnicos',
+            hash: 't_1_3',
+          },
+          {
+            numero: '1.4',
+            titulo: 'Ordenamiento de datos, indexación y recuperación',
+            hash: 't_1_4',
+          },
+        ],
       },
       {
         nombreRuta: 'tema2',
         numero: '2',
         titulo: 'Preparación de datos',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.2',
+            titulo: 'Entendimiento de la data',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Detección de errores y datos faltantes',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: 'Identificación de variables importantes',
+            hash: 't_2_3',
+          },
+          {
+            numero: '2.4',
+            titulo: 'Dataset',
+            hash: 't_2_4',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
         titulo: 'La inteligencia de negocios',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '3.1',
+            titulo: 'Identificación de las preguntas básicas',
+            hash: 't_3_1',
+          },
+          {
+            numero: '3.2',
+            titulo: 'Metodología de integración',
+            hash: 't_3_2',
+          },
+          {
+            numero: '3.3',
+            titulo: 'Herramientas de administración',
+            hash: 't_3_3',
+          },
+          {
+            numero: '3.4',
+            titulo: 'Técnicas de solución de problemas (modelación de datos)',
+            hash: 't_3_4',
+          },
+          {
+            numero: '3.5',
+            titulo: 'Metodologías de análisis (Kimball, Inmon)',
+            hash: 't_3_5',
+          },
+          {
+            numero: '3.6',
+            titulo: 'Verificación de valores y escalas',
+            hash: 't_3_6',
+          },
+          {
+            numero: '3.7',
+            titulo: 'Procedimientos almacenados y funciones',
+            hash: 't_3_7',
+          },
+          {
+            numero: '3.8',
+            titulo: 'Disparadores',
+            hash: 't_3_8',
+          },
+        ],
       },
       {
         nombreRuta: 'tema4',
         numero: '4',
         titulo: 'Análisis exploratorio de datos',
         desarrolloContenidos: true,
-        subMenu: [],
+        subMenu: [
+          {
+            numero: '4.1',
+            titulo: 'Estadística descriptiva y estadística inferencial',
+            hash: 't_4_1',
+          },
+          {
+            numero: '4.2',
+            titulo: 'Población y muestra',
+            hash: 't_4_2',
+          },
+          {
+            numero: '4.3',
+            titulo: 'Escalas de medida y clasificación de variable',
+            hash: 't_4_3',
+          },
+          {
+            numero: '4.4',
+            titulo: 'Técnicas de análisis estadístico',
+            hash: 't_4_4',
+          },
+        ],
       },
       {
         nombreRuta: 'tema5',
-        icono: 'far fa-file-alt',
         numero: '5',
         titulo: 'Métodos para hacer análisis exploratorio de adtos',
         desarrolloContenidos: true,
-        subMenu: [],
+        subMenu: [
+          {
+            numero: '5.1',
+            titulo: 'Datos univariables',
+            hash: 't_5_1',
+          },
+          {
+            numero: '5.2',
+            titulo: 'Datos bivariados',
+            hash: 't_5_2',
+          },
+          {
+            numero: '5.3',
+            titulo: 'Datos multivariables',
+            hash: 't_5_3',
+          },
+          {
+            numero: '5.4',
+            titulo: 'Reglas de negocio',
+            hash: 't_5_4',
+          },
+          {
+            numero: '5.5',
+            titulo: 'Tipo de restricciones',
+            hash: 't_5_5',
+          },
+          {
+            numero: '5.6',
+            titulo: 'Programación transaccional',
+            hash: 't_5_6',
+          },
+          {
+            numero: '5.7',
+            titulo:
+              'Programación de estructuras no lineales, desnormalización, series y dataframes',
+            hash: 't_5_7',
+          },
+          {
+            numero: '5.8',
+            titulo: 'Álgebra relacional',
+            hash: 't_5_8',
+          },
+          {
+            numero: '5.9',
+            titulo: 'SQL',
+            hash: 't_5_9',
+          },
+          {
+            numero: '5.10',
+            titulo: 'No SQL',
+            hash: 't_5_10',
+          },
+          {
+            numero: '5.11',
+            titulo: 'JSON, BSON y XML',
+            hash: 't_5_11',
+          },
+          {
+            numero: '5.12',
+            titulo: 'DDL, DML, DCL',
+            hash: 't_5_12',
+          },
+        ],
       },
       {
         nombreRuta: 'tema6',
-        icono: 'far fa-file-alt',
         numero: '6',
         titulo: 'Estructura y componentes de analítica de datos',
         desarrolloContenidos: true,
-        subMenu: [],
+        subMenu: [
+          {
+            numero: '6.1',
+            titulo: 'Bodega de datos',
+            hash: 't_6_1',
+          },
+          {
+            numero: '6.2',
+            titulo: 'Tipos estrella',
+            hash: 't_6_2',
+          },
+          {
+            numero: '6.3',
+            titulo: 'Copo de nieve',
+            hash: 't_6_3',
+          },
+          {
+            numero: '6.4',
+            titulo: 'Constelación',
+            hash: 't_6_4',
+          },
+        ],
       },
       {
         nombreRuta: 'tema7',
         numero: '7',
         titulo: 'Herramientas para el análisis de datos ',
         desarrolloContenidos: true,
-        subMenu: [],
+        subMenu: [
+          {
+            numero: '7.1',
+            titulo: 'Entornos de desarrollo - IDE',
+            hash: 't_7_1',
+          },
+          {
+            numero: '7.2',
+            titulo: 'Python',
+            hash: 't_7_2',
+          },
+          {
+            numero: '7.3',
+            titulo: 'Librerías',
+            hash: 't_7_3',
+          },
+        ],
       },
     ],
     subMenu: [
@@ -84,12 +287,6 @@ export default {
         icono: 'fas fa-sitemap',
         titulo: 'Síntesis',
         nombreRuta: 'sintesis',
-        desarrolloContenidos: true,
-      },
-      {
-        nombreRuta: 'actividad',
-        icono: 'far fa-question-circle',
-        titulo: 'Actividad didáctica',
         desarrolloContenidos: true,
       },
       {
@@ -168,43 +365,68 @@ export default {
   ],
   glosario: [
     {
-      termino: 'Ágil',
+      termino: 'Información Descriptiva',
       significado:
-        'Comprende un conjunto de tareas o acciones que se utilizan para producir y mantener productos, así como para lograr los objetivos del proceso. La actividad incluye los procedimientos, los estándares, las políticas y los objetivos para crear y modificar un conjunto de productos de trabajo.',
+        'se refiere a las historias que los datos cuentan, se refiere a un histórico de eventos y resultados.',
     },
     {
-      termino: 'Capa de red',
+      termino: 'Información Predictiva ',
       significado:
-        'Es la capa tres en del modelo OSI, se encarga de permitir la conexión entre dispositivos que están ubicados en redes diferentes. Es la capa de direccionamiento.',
+        'se refiere a los hechos probables que sucederán, esto se realiza basado en datos descriptivos, es decir, datos históricos y procesos matemáticos y/o modelación de Machine Learning.',
     },
     {
-      termino: 'Ciclo de vida <i>software</i>',
+      termino: 'Joins ',
       significado:
-        'Aplicación de metodologías para el desarrollo del sistema <i>software</i> (AECC, 1986).',
+        'proceso de selección de datos de varias tablas y unirlos en una vista o nueva tabla. Una instrucción de “SQL JOIN” en un comando Select que combina columnas entre una o más tablas en una base de datos.',
     },
     {
-      termino: 'Método',
+      termino: 'Lenguaje DAX ',
       significado:
-        'Indica cómo construir técnicamente el <i>software</i>. Se incluyen técnicas de modelado y otras técnicas descriptivas.',
+        'lenguaje específico para gestión de datos creado por Microsoft (Data Analysis Expressions). Se emplea en colecciones de datos en aplicaciones como Excel, Analysis Services y Power BI.',
     },
     {
-      termino: 'Metodología',
-      significado: 'Colección de métodos para resolver un tipo de problemas.',
+      termino: 'Lenguaje R ',
+      significado:
+        'lenguaje de programación para la gestión de datos. Es un lenguaje interpretado que ejecuta las instrucciones directamente sin previa compilación.',
     },
     {
-      termino: 'Protocolo de comunicación',
+      termino: 'Normalización ',
       significado:
-        'Conjunto de reglas que permiten la comunicación entre dos o más nodos en servicios específicos. En telemática los protocolos más usados son HTTP, FTP, SMTP, DNS, etc.',
+        'la estructura organizada en datos relacionales que cumplen unas reglas de normalización que garantizan la integridad, calidad y optimización en la base de datos.',
     },
     {
-      termino: 'Requerimiento',
+      termino: 'Procesadores ',
       significado:
-        'Se refiere a la petición que se hace de algo que se solicita.',
+        'CPU (Unidad central de proceso), es el componente del computador y otros dispositivos programables, que interpreta las instrucciones contenidas en los programas y procesa los datos.',
     },
     {
-      termino: 'Requisito',
+      termino: 'Procesamiento por lotes ',
       significado:
-        'Condición que debe cumplir algo, en general el requisito cumple con lo que se necesita con el requerimiento.',
+        'al tener muchas cargas de datos y procesamientos, es necesario reunir recursos para que se ejecuten de manera independiente optimizando recursos, de esta manera las tareas se completan periódicamente de manera repetitiva',
+    },
+    {
+      termino: 'RAM ',
+      significado:
+        'es la memoria temporal o de corto plazo de las computadoras, es la memoria principal de trabajo, los programas y datos se cargan allí para que trabajen más rápidamente.',
+    },
+    {
+      termino: 'Script ',
+      significado:
+        'se refiere a fragmentos de código de programación que pueden ejecutar una o varias funciones.',
+    },
+    {
+      termino: 'Sistema operativo ',
+      significado:
+        'es el software principal de las computadoras, se emplea como plataforma para gestionar las aplicaciones, recursos del hardware y entornos gráficos y funcionales.',
+    },
+    {
+      termino: 'Tabular ',
+      significado:
+        'en estadística, son la recopilación y procesamiento de la información capturada de los instrumentos disponibles al momento de realizar encuestas, toma de datos y otras.',
+    },
+    {
+      termino: 'TI ',
+      significado: '(IT) Abreviatura de Tecnología de la información.',
     },
   ],
   referencias: [
